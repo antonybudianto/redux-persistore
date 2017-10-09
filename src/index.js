e62e4f3b621fb349ed56d8cbece732cfa552db22
Antony Budianto <antonybudianto@gmail.com>
2017-10-09 07:45:05 +0700

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-import * as _ from 'lodash'
+import * as _ from 'lodash';
 
 const defaultConfig = {
   list: [],
   prefix: ''
-}
+};
 
 export function persistStore (store, config = defaultConfig) {
-  const { list, prefix } = config
-  let prevState = null
+  const { list, prefix } = config;
+  let prevState = null;
   store.subscribe(() => {
-    const state = store.getState()
+    const state = store.getState();
 
     if (prevState === state) {
-      return
+      return;
     }
 
     list.forEach(p => {
-      let currentValue = _.get(state, p.path)
-      const prevValue = _.get(prevState, p.path)
+      let currentValue = _.get(state, p.path);
+      const prevValue = _.get(prevState, p.path);
       if (currentValue !== prevValue) {
         if (currentValue instanceof Array) {
-          currentValue = JSON.stringify(currentValue)
+          currentValue = JSON.stringify(currentValue);
         }
-        let path = [prefix, ...p.path.split('.')].join('-')
-        localStorage.setItem(path, currentValue)
+        let path = [prefix, ...p.path.split('.')].join('-');
+        localStorage.setItem(path, currentValue);
       }
-    })
+    });
 
-    prevState = state
-  })
+    prevState = state;
+  });
 }
 
 export function getInitialState (config = defaultConfig) {
-  const { list, prefix } = config
-  const initialState = {}
+  const { list, prefix } = config;
+  const initialState = {};
   list.forEach(p => {
-    const path = [prefix, ...p.path.split('.')].join('-')
-    let data = localStorage.getItem(path)
+    const path = [prefix, ...p.path.split('.')].join('-');
+    let data = localStorage.getItem(path);
     if (data) {
       if (p.isArray) {
-        data = JSON.parse(data)
+        data = JSON.parse(data);
       }
-      _.set(initialState, p.path, data)
+      _.set(initialState, p.path, data);
     }
-  })
-  return initialState
+  });
+  return initialState;
 }
